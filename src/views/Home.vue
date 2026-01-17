3ae6f4b948ccfc9faaca04ba06a6cd3b42c08ce9
mariecloe28 <marie.cloe@hotmail.com>
2026-01-17 20:52:40 +0000

    Add path for the Home page in index.js

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mt-5 text-center">
+        <h1>Hello!</h1>
+    <button class="btn btn-success">A Bootstrap Button</button>
+  </div>
+</template>
+
+<script setup>
+    name: Home
+</script>
