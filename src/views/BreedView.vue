6ec8029ed09853071f69faf037a607f051f963c6
mariecloe28 <marie.cloe@hotmail.com>
2026-01-31 21:24:22 +0000

    Add some CSS for the BreedView

--- src/views/BreedView.vue
+++ src/views/BreedView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div v-if="breed">
-    <p>Name : {{ breed.name }}</p>
-    <p>Description : {{ breed.description }}</p>
-    <p>Hypoallergenic :<span v-if="breed.hypoallergenic"> Yes </span> <span v-else> No </span></p>
-    <p>Life Range : {{ breed.life.min }} to {{ breed.life.max }} years</p>
-    <p>Male Weight : {{ breed.male_weight.min }} to {{ breed.male_weight.max }} kg</p>
-    <p>Female Weight : {{ breed.female_weight.min }} to {{ breed.female_weight.max }} kg</p>
+  <div class="breed text-center" v-if="breed">
+    <p class="name">Name : {{ breed.name }}</p>
+    <p class="description">Description : {{ breed.description }}</p>
+    <p class="hypoallergenic" >Hypoallergenic :<span v-if="breed.hypoallergenic"> Yes </span> <span v-else> No </span></p>
+    <p class="life-range">Life Range : {{ breed.life.min }} to {{ breed.life.max }} years</p>
+    <p class="male-weight">Male Weight : {{ breed.male_weight.min }} to {{ breed.male_weight.max }} kg</p>
+    <p class="female-weight">Female Weight : {{ breed.female_weight.min }} to {{ breed.female_weight.max }} kg</p>
   </div>
   <div v-else>Loading...</div>
 </template>
@@ -27,4 +27,17 @@ onMounted(async () => {
 
 </script>
 
-<style></style>
+<style scoped>
+  .breed{
+    justify-content: center;
+    background-color: blueviolet;
+    font-size: large;
+  }
+  body {
+    align-content: center;
+  }
+  .description {
+    display: flex;
+    flex-direction: warp;
+  }
+</style>
